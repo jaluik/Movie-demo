3e9ff83621e07506682d968e67f2faec5bc7516f
jaluik <cqzhengmouren@gmail.com>
2019-04-24 23:49:06 +0800

    dataseted

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -3,81 +3,22 @@
         <div class="city_list">
             <div class="city_hot">
                 <h2>热门城市</h2>
-                <ul class="clearfix">
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
+                <ul class="clearfix" >
+                    <li v-for="(city,index) in hotList" :key ="index">{{city}}</li>
                 </ul>
             </div>
-            <div class="city_sort">
-                <div>
-                    <h2>A</h2>
+            <div class="city_sort" ref = "city_sort" >
+                <div v-for ="(item,index) in cityList" :key = "item.index">
+                    <h2>{{item.index}}</h2>
                     <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
+                        <li v-for= "(city,key) in item.list">{{city.nm}}</li>
                     </ul>
                 </div>	
             </div>
         </div>
         <div class="city_index">
             <ul>
-                <li>A</li>
-                <li>B</li>
-                <li>C</li>
-                <li>D</li>
-                <li>E</li>
+                <li  v-for="(item,index) in cityList" @touchstart = "handleToIndex(index)">{{item.index}}</li>
             </ul>
         </div>
     </div>
@@ -85,7 +26,71 @@
 
 <script>
 export default {
-    name : 'City'
+    name : 'City',
+    data(){
+        return {
+            hotList: [],
+            cityList: [],
+            
+        }
+    },
+    mounted(){
+        this.axios.get('/api/cityList').then((res) =>{
+            if(res.data.msg == 'ok'){
+               var cities = res.data.data.cities;
+               [this.cityList, this.hotList] = this.formatCitylist(cities)             
+               
+            }
+        })
+    },
+    methods:{
+        formatCitylist(cities){
+            var cityList =[];
+            var hotList = [];
+            // 转换的样式   [{index: 'B',list: [{nm: '北京', id: 1}]}]
+            for(var i = 0; i < cities.length; i++){
+                if(cities[i].isHot == 1){
+                    hotList.push(cities[i].nm)
+                }
+                var firstLeter = cities[i].py.substring(0,1).toUpperCase();
+                if(toCom(firstLeter)){  //已添加
+                for(var j =0; j < cityList.length; j ++){
+                    if(cityList[j].index == firstLeter){
+                        cityList[j].list.push({nm: cities[i].nm, id: cities[i].id})
+
+                    }
+                }
+                }
+                else{  //未添加
+                    cityList.push({index: firstLeter, list:[{nm: cities[i].nm, id: cities[i].id}]})
+                }
+            };
+           
+            function toCom(firstLeter){
+                for(var i = 0;i <cityList.length; i ++){
+                    if (cityList[i].index == firstLeter){
+                        return true
+                    }
+               
+                }     
+                return false
+
+            }
+            cityList.sort((a,b) =>{
+                if(a.index > b.index){
+                    return 1
+                }else{
+                    return -1
+                }
+
+            })
+           return [cityList, hotList ]
+        },
+        handleToIndex(index){
+            var h2 = this.$refs.city_sort.getElementsByTagName('h2');
+            this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+        }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './routers'
 import store from './stores'
 
+import axios from 'axios'
+Vue.prototype.axios  = axios
+
 Vue.config.productionTip = false
 
 new Vue({
